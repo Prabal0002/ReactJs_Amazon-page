f69fcf80e604b44af4e6082d3e55de5c628a9ceb
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-07-11 18:55:07 +0530

    Update Product.jsx

--- Product.jsx
+++ Product.jsx
@@ -1,1 +1,21 @@
+import Price from './Price.jsx';
+import "./Product.css";
+
+
+export default function Product ({title, idx}){
+
+
+  
+    let oldPrice=["5000","6000","7000","8000"];
+  let newPrice=["4000","5000","6000","7000"];
+  let discription=["Samsung phone","vivo phone","oppo phone","mi phone"  ];
+return (
+  <div class = "product">
+  <h4> {title}</h4>
+  <p>{discription [idx]}</p>
+  <Price oldprices={oldPrice[idx]} newprices={newPrice[idx]} />
+  </div>
+
+    
+  );
 
