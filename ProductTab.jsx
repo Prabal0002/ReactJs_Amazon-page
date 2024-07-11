bbe750bff4027f4fa2deffefe5258715edd23adc
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-07-11 18:38:48 +0530

    Update ProductTab.jsx

--- ProductTab.jsx
+++ ProductTab.jsx
@@ -1,1 +1,22 @@
+import Product from './Product.jsx';
 
+export default function ProductTab(){
+  let style ={
+  display:"flex",
+  flexBox:"wrap",
+  justifyContent:"center",
+  alignItems:"center",
+}
+    return(
+<div style={style}>
+  <Product title="samsung" idx={0} />
+    <Product title="vivo" idx={1} />
+
+  <Product title="oppo" idx={2}/>
+
+  <Product title="mi" idx={3} />
+</div>
+  
+);
+
+}
