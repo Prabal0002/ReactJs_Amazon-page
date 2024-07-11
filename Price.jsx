b55e2b8ebcc79b56ccf61662b6b864b75656568a
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-07-11 18:48:18 +0530

    Update Price.jsx

--- Price.jsx
+++ Price.jsx
@@ -1,1 +1,22 @@
+export default function Price({oldprices,newprices}){
+let oldStyle={
+  textDecoration:"line-through",
+};
+  let newStyle={
+    fontWeight:"bold",
+  };
+  let style={
+    backgroundColor:"#e0c367",
+    height:"30px",
+    borderBottomLeftRadius:"14px",
+    borderBottomRightRadius:"14px",
+  };
+return(
+  <div style={style}>
+    <spam style={oldStyle}>{oldprices}</spam>
+    &nbsp;&nbsp;nbsp;
+    <spam style={newStyle}>{newprices}</spam>
+      </div>
+      );
 
+}
