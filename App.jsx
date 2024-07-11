1cef63e6ee44df0f80a8530540a9b85bd3f293b5
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-07-11 18:24:20 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import ProductTab from './ProductTab.js';
+
+
+function App(){
+  
+    return (
+      <div className="App">
+        <h1> my card</h1>
+        <ProductTab />
+          
+      </div>
+    );
+  }
+
+
+export default App;
