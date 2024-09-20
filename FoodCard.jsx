79e81dd41d916a982c48da87d432f1387a751ff0
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-09-20 23:25:19 +0530

    Update FoodCard.jsx

--- FoodCard.jsx
+++ FoodCard.jsx
@@ -4,19 +4,19 @@ import React, { useState } from 'react';
 function FoodCard() {
   const [showCard, setShowCard] = useState(0);
   const burger= "https://www.vecteezy.com/photo/23558535-close-up-of-a-juicy-burger-with-fries-it-look-very-delicious-big-sandwich-hamburger-with-juicy-beef-burger-cheese-tomato-and-red-onion"
-  const pizza = "";
+  const pizza = "https://www.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_7572612.htm#query=pizza&position=3&from_view=keyword&track=ais_hybrid&uuid=b89762d2-e636-4d12-8d5d-17e5cd86bfe3";
   const foodCards = [
     {
       image:burger,
-      name: 'Food 1',
-      description: 'Description 1',
-      price: 10.99,
+      name: 'Burger',
+      description: 'this is testy burger',
+      price: 10.99$,
     },
     {
       image:pizza,
-      name: 'Food 2',
-      description: 'Description 2',
-      price: 9.99,
+      name: 'Pizza ',
+      description: 'this is testy pizza',
+      price: 9.99$,
     },
   ];
 
