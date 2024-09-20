a12f58c25d57797a6af8449a353d6454276cefa8
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-09-20 23:11:59 +0530

    Update FoodCard.jsx

--- FoodCard.jsx
+++ FoodCard.jsx
@@ -3,14 +3,17 @@ import React, { useState } from 'react';
 
 function FoodCard() {
   const [showCard, setShowCard] = useState(0);
-
+  const burger= "https://www.vecteezy.com/photo/23558535-close-up-of-a-juicy-burger-with-fries-it-look-very-delicious-big-sandwich-hamburger-with-juicy-beef-burger-cheese-tomato-and-red-onion"
+  const pizza = "";
   const foodCards = [
     {
+      image:burger,
       name: 'Food 1',
       description: 'Description 1',
       price: 10.99,
     },
     {
+      image:pizza,
       name: 'Food 2',
       description: 'Description 2',
       price: 9.99,
@@ -23,7 +26,7 @@ function FoodCard() {
         <div key={index}>
           {showCard === index && (
             <div>
-              
+              <image>{food.image}</image>
               <h2>{food.name}</h2>
               <p>{food.description}</p>
               <p>Price: ${food.price}</p>
